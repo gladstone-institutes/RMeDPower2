3fba9a83c79bf6af055625150544561101942ed9
Reuben Thomas <rthomas@LAXUCR09100.gladstone.internal>
2025-08-11 15:25:27 -0700

    update getEstimatesOfInterest

--- R/getEstimatesOfInterest.R
+++ R/getEstimatesOfInterest.R
@@ -28,13 +28,23 @@ getEstimatesOfInterest <- function(data, design, model) {
   alpha=design@outlier_alpha
   na.action=design@na_action
 
+  # New enhanced parameters
+  include_interaction = design@include_interaction
+  random_slope_variable = design@random_slope_variable
+
+
+  # cat("\n=== Enhanced Estimates of Interest Analysis ===\n")
+  # cat("Model specifications:\n")
+  # cat("- Interaction effects:", ifelse(include_interaction, "Yes", "No"), "\n")
+  # cat("- Random slopes:", ifelse(!is.null(random_slope_variable), "Yes", "No"), "\n")
+
   res <- list()
   ##get estimates
   res[[1]] <- calculate_lmer_estimates(data, condition_column, experimental_columns, response_column, total_column, condition_is_categorical, covariate,
-                                                 crossed_columns, error_is_non_normal, family_p, na.action)
+                                                 crossed_columns, error_is_non_normal, family_p, na.action, include_interaction, random_slope_variable)
   ##visualize estimates
   res[[2]] <- get_residuals(data, condition_column, experimental_columns, response_column, condition_is_categorical, covariate,
-                                                crossed_columns, total_column, error_is_non_normal, family_p, na.action)
+                                                crossed_columns, total_column, error_is_non_normal, family_p, na.action, include_interaction, random_slope_variable)
 
   return(res)
 
