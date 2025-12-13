5014a0fdeab10e1b240383da1a5b3eed75125c05
reubenthomas <dr.reuben.thomas@gmail.com>
2025-12-13 11:35:20 -0800

    fix error model_fit negative binomial family

--- R/get_model_and_data.R
+++ R/get_model_and_data.R
@@ -382,8 +382,7 @@ generate_model_fit <- function(data, fixed_formula, random_formula,
   }
   else if (!is.null(family_p) && family_p$family == "negative_binomial" && is.null(total_column)) {
     # Negative binomial with offset
-    formula_str <<- as.formula(paste("response_column ~", fixed_formula, "+", random_formula,
-                                     ")"))
+    formula_str <<- as.formula(paste("response_column ~", fixed_formula, "+", random_formula))
     lmerFit <- lme4::glmer.nb(formula_str, data = data, family = family_p)
   }
   else {
